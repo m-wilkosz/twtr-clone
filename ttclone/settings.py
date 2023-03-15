fb4c0f35d8923133660d4bb610056021186e34ea
wilkosz <wilkosz1998@gmail.com>
2023-03-15 17:16:25 +0100

    tweet form drf serializer

--- ttclone/settings.py
+++ ttclone/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 LOGIN_URL = '/login'
-
+MAX_TWEET_LENGTH = 240
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # third-party
+    'rest_framework',
+    # internal
     'tweets',
 ]
 
