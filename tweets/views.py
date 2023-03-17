c88232b379d0e405fb80364456de5347a843580d
wilkosz <wilkosz1998@gmail.com>
2023-03-17 17:08:21 +0100

    retweeting logic

--- tweets/views.py
+++ tweets/views.py
@@ -64,6 +64,7 @@ def tweet_action_view(request, *args, **kwargs):
         data = serializer.validated_data
         tweet_id = data.get('id')
         action = data.get('action')
+        content = data.get('content')
         qs = Tweet.objects.filter(id=tweet_id)
         if not qs.exists():
             return Response({}, status=404)
@@ -75,8 +76,9 @@ def tweet_action_view(request, *args, **kwargs):
         elif action == 'unlike':
             obj.likes.remove(request.user)
         elif action == 'retweet':
-            # todo
-            pass
+            new_tweet = Tweet.objects.create(user=request.user, parent=obj, content=content)
+            serializer = TweetSerializer(new_tweet)
+            return Response(serializer.data, status=200)
     return Response({}, status=200)
 
 def tweet_create_view_pure_django(request, *args, **kwargs):
