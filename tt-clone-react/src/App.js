3bf204904fccf12adaa0f61277240b9778c1f9bb
wilkosz <wilkosz1998@gmail.com>
2023-03-22 15:55:53 +0100

    action btn, react app divided into modules

--- tt-clone-react/src/App.js
+++ tt-clone-react/src/App.js
@@ -1,44 +1,9 @@
 import {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function loadTweets(callback) {
-  const xhr = new XMLHttpRequest()
-  const method = 'GET'
-  const url = 'http://localhost:8000/api/tweets/'
-  const responseType = 'json'
-  xhr.responseType = responseType
-  xhr.open(method, url)
-  xhr.onload = function() {
-      callback(xhr.response, xhr.status)
-  }
-  xhr.onerror = function(e) {
-    console.log(e)
-    callback({'message': 'the request was an error'}, 400)
-  }
-  xhr.send()
-}
-
-function Tweet(props) {
-  const {tweet} = props
-  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
-  return <div className={className}>
-    <p>{tweet.id} - {tweet.content}</p>
-  </div>
-}
+import {TweetsList} from './tweets'
 
 function App() {
-  const [tweets, setTweets] = useState([])
-  useEffect(() => {
-    const callback = (response, status) => {
-      if (status === 200) {
-        setTweets(response)
-      } else {
-        alert('there was an error')
-      }
-    }
-    loadTweets(callback)
-  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -47,9 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          {tweets.map((item, index)=>{
-            return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
-          })}
+          <TweetsList />
         </div>
         <a
           className="App-link"
