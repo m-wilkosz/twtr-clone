f9fcbce4a520d14f0f98cd7cbc4eca3de5dd9e75
wilkosz <wilkosz1998@gmail.com>
2023-03-24 11:57:56 +0100

    rendering react app via dj template

--- tt-clone-react/src/index.js
+++ tt-clone-react/src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { TweetsComponent } from './tweets';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const appEl = document.getElementById('root')
+if (appEl) {
+  const root = ReactDOM.createRoot(appEl);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+const tweetsEl = document.getElementById('tweets-react')
+if (tweetsEl) {
+  const root = ReactDOM.createRoot(tweetsEl);
+  root.render(
+    <React.StrictMode>
+      <TweetsComponent />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
