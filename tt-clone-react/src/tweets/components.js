edecead792ad9ea50d929ae8adf951fe51ba365f
wilkosz <wilkosz1998@gmail.com>
2023-03-24 14:13:00 +0100

    tweets xhr lookup improved

--- tt-clone-react/src/tweets/components.js
+++ tt-clone-react/src/tweets/components.js
@@ -30,6 +30,7 @@ export function TweetsComponent(props) {
 export function TweetsList(props) {
     const [tweetsInit, setTweetsInit] = useState([])
     const [tweets, setTweets] = useState([])
+    const [tweetsDidSet, setTweetsDidSet] = useState(false)
     useEffect(() => {
       const final = [...props.newTweets].concat(tweetsInit)
       if (final.length !== tweets.length) {
@@ -37,15 +38,18 @@ export function TweetsList(props) {
       }
     }, [props.newTweets, tweets, tweetsInit])
     useEffect(() => {
-      const callback = (response, status) => {
-        if (status === 200) {
-          setTweetsInit(response)
-        } else {
-          alert('there was an error')
+      if (tweetsDidSet === false) {
+        const callback = (response, status) => {
+          if (status === 200) {
+            setTweetsInit(response)
+            setTweetsDidSet(true)
+          } else {
+            alert('there was an error')
+          }
         }
+        loadTweets(callback)
       }
-      loadTweets(callback)
-    }, [tweetsInit])
+    }, [tweetsInit, tweetsDidSet, setTweetsDidSet])
     return tweets.map((item, index)=>{
       return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
     })
