54e087cef4e1024f6becf1c711ca643b0746761e
wilkosz <wilkosz1998@gmail.com>
2023-03-30 11:54:40 +0200

    dj views refactoring

--- ttclone/urls.py
+++ ttclone/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 from tweets.views import tweets_list_view, tweets_detail_view, tweets_profile_view
 
@@ -26,7 +25,7 @@ urlpatterns = [
     path("", tweets_list_view),
     path("<int:tweet_id>", tweets_detail_view),
     path("profile/<str:username>", tweets_profile_view),
-    path("api/tweets/", include("tweets.urls"))
+    path("api/tweets/", include("tweets.api.urls"))
 ]
 
 if settings.DEBUG:
