34c89d9e6052f1caa8f66414e673b7e47a5cb633
wilkosz <wilkosz1998@gmail.com>
2023-03-31 10:39:21 +0200

    authentication & registration

--- ttclone/urls.py
+++ ttclone/urls.py
@@ -18,11 +18,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts.views import (login_view, logout_view, register_view)
 from tweets.views import tweets_list_view, tweets_detail_view, tweets_profile_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", tweets_list_view),
+    path("login/", login_view),
+    path("logout/", logout_view),
+    path("register/", register_view),
     path("<int:tweet_id>", tweets_detail_view),
     path("profile/<str:username>", tweets_profile_view),
     path("api/tweets/", include("tweets.api.urls"))
