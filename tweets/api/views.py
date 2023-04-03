9e458b4dac2589f3860011f9ef23ae47f0f77bfb
wilkosz <wilkosz1998@gmail.com>
2023-04-03 16:43:26 +0200

    tweets pagination method

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -5,6 +5,7 @@ from django.utils.http import is_safe_url
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from ..forms import TweetForm
 from ..models import Tweet
@@ -28,16 +29,21 @@ def tweet_list_view(request, *args, **kwargs):
     username = request.GET.get("username")
     if username != None:
         qs = qs.by_username(username)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data)
+    return get_paginated_queryset_response(qs, request)
+
+def get_paginated_queryset_response(qs, request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 20
+    paginated_qs = paginator.paginate_queryset(qs, request)
+    serializer = TweetSerializer(paginated_qs, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def tweet_feed_view(request, *args, **kwargs):
     user = request.user
     qs = Tweet.objects.feed(user)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data)
+    return get_paginated_queryset_response(qs, request)
 
 @api_view(["GET"])
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
