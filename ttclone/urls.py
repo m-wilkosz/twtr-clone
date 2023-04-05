10ede429ce85702b4755b1bde1db3756cf23b39b
wilkosz <wilkosz1998@gmail.com>
2023-04-05 17:10:12 +0200

    new react build (feed) in dj

--- ttclone/urls.py
+++ ttclone/urls.py
@@ -19,11 +19,12 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 from accounts.views import (login_view, logout_view, register_view)
-from tweets.views import tweets_list_view, tweets_detail_view
+from tweets.views import home_view, tweets_list_view, tweets_detail_view
 
 urlpatterns = [
+    path("", home_view),
     path("admin/", admin.site.urls),
-    path("", tweets_list_view),
+    path("global/", tweets_list_view),
     path("login/", login_view),
     path("logout/", logout_view),
     path("register/", register_view),
