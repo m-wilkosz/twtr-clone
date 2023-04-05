8ab3a9f259af67b3ebeced45a58e90c5809ed57e
wilkosz <wilkosz1998@gmail.com>
2023-04-05 20:36:14 +0200

    passing request to serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,13 +4,23 @@ from .models import Profile
 class PublicProfileSerializer(serializers.ModelSerializer):
     first_name = serializers.SerializerMethodField(read_only=True)
     last_name = serializers.SerializerMethodField(read_only=True)
+    is_following = serializers.SerializerMethodField(read_only=True)
     username = serializers.SerializerMethodField(read_only=True)
     followers_count = serializers.SerializerMethodField(read_only=True)
     following_count = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Profile
-        fields = ["first_name", "last_name", "id", "bio", "location", "followers_count", "following_count", "username"]
+        fields = ["first_name", "last_name", "id", "bio", "location", "followers_count", "following_count", "is_following", "username"]
+
+    def get_is_following(self, obj):
+        is_following = False
+        context = self.context
+        request = context.get("request")
+        if request:
+            user = request.user
+            is_following = user in obj.followers.all()
+        return is_following
 
     def get_first_name(self, obj):
         return obj.user.first_name
