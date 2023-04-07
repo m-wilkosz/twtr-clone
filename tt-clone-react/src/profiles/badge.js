07bc221f61be99a766b93ee52feb9ba5c5c5e2bb
wilkosz <wilkosz1998@gmail.com>
2023-04-07 17:06:27 +0200

    follower count display

--- tt-clone-react/src/profiles/badge.js
+++ tt-clone-react/src/profiles/badge.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react"
 import {apiProfileDetail, apiProfileFollowToggle} from "./lookup"
 import {UserDisplay, UserPicture} from "./components"
+import {DisplayCount} from "./utils"
 
 function ProfileBadge(props) {
     const {user, didFollowToggle, profileLoading} = props
@@ -15,6 +16,10 @@ function ProfileBadge(props) {
     return user ? <div>
             <UserPicture user={user} hideLink />
             <p><UserDisplay user={user} includeFullName hideLink /></p>
+            <p><DisplayCount>{user.followers_count}</DisplayCount> {user.followers_count === 1 ? "follower" : "followers"}</p>
+            <p><DisplayCount>{user.following_count}</DisplayCount> following</p>
+            <p>{user.location}</p>
+            <p>{user.bio}</p>
             <button className="btn btn-primary" onClick={handleFollowToggle}>{currentVerb}</button>
         </div> : null
 }
