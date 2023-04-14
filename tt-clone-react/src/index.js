92108ff1567b7c078f010cf0a809d6368127fddd
m-wilkosz <wilkosz1998@gmail.com>
2023-04-14 11:23:43 +0200

    sidebar component

--- tt-clone-react/src/index.js
+++ tt-clone-react/src/index.js
@@ -1,46 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {ProfileBadgeComponent} from "./profiles";
-import {FeedComponent, TweetsComponent, TweetDetailComponent} from './tweets';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import {ProfileBadgeComponent} from "./profiles"
+import {FeedComponent, TweetsComponent, TweetDetailComponent} from "./tweets"
+import SidebarComponent from "./sidebar/sidebar"
+import reportWebVitals from "./reportWebVitals"
 
-const appEl = document.getElementById('root')
+const appEl = document.getElementById("root")
 if (appEl) {
-  const root = ReactDOM.createRoot(appEl);
+  const root = ReactDOM.createRoot(appEl)
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  );
+  )
 }
+
 const e = React.createElement
-const tweetsEl = document.getElementById('tweets-react')
+const tweetsEl = document.getElementById("tweets-react")
 if (tweetsEl) {
-  const root = ReactDOM.createRoot(tweetsEl);
-  root.render(e(TweetsComponent, tweetsEl.dataset));
+  const root = ReactDOM.createRoot(tweetsEl)
+  root.render(e(TweetsComponent, tweetsEl.dataset))
+}
+const sidebarEl = document.getElementById("sidebar")
+if (sidebarEl) {
+  const root = ReactDOM.createRoot(sidebarEl)
+  root.render(
+    <React.StrictMode>
+      <SidebarComponent />
+    </React.StrictMode>
+  )
 }
 
-const tweetsFeedEl = document.getElementById('tweets-react-feed')
+const tweetsFeedEl = document.getElementById("tweets-react-feed")
 if (tweetsFeedEl) {
-  const root = ReactDOM.createRoot(tweetsFeedEl);
-  root.render(e(FeedComponent, tweetsFeedEl.dataset));
+  const root = ReactDOM.createRoot(tweetsFeedEl)
+  root.render(e(FeedComponent, tweetsFeedEl.dataset))
 }
 
-const tweetDetailElements = document.querySelectorAll('.tweet-detail')
+const tweetDetailElements = document.querySelectorAll(".tweet-detail")
 tweetDetailElements.forEach(container => {
-  const root = ReactDOM.createRoot(container);
-  root.render(e(TweetDetailComponent, container.dataset));
+  const root = ReactDOM.createRoot(container)
+  root.render(e(TweetDetailComponent, container.dataset))
 })
 
-const userProfileBadgeElements = document.querySelectorAll('.tweet-react-badge')
+const userProfileBadgeElements = document.querySelectorAll(".tweet-react-badge")
 userProfileBadgeElements.forEach(container => {
-  const root = ReactDOM.createRoot(container);
-  root.render(e(ProfileBadgeComponent, container.dataset));
+  const root = ReactDOM.createRoot(container)
+  root.render(e(ProfileBadgeComponent, container.dataset))
 })
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
---
+++ tt-clone-react/src/sidebar/sidebar.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const SidebarComponent = () => {
+  return (
+    <div className="sidebar">
+      <h3>Sidebar</h3>
+      <ul>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>Notifications</li>
+        <li>Messages</li>
+        <li>Bookmarks</li>
+        <li>Profile</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  )
+}
+
+export default SidebarComponent
