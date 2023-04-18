1972971a75d7158357315d7c24c9eb33f46fe1ee
m-wilkosz <wilkosz1998@gmail.com>
2023-04-18 08:54:20 +0200

    completed unit tests for tweets app

--- tweets/tests.py
+++ tweets/tests.py
@@ -7,28 +7,28 @@ User = get_user_model()
 
 class TweetTestCase(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='abc', password='password')
-        self.secondUser = User.objects.create_user(username='xyz', password='password')
-        Tweet.objects.create(content='first tweet', user=self.user)
-        Tweet.objects.create(content='second tweet', user=self.user)
-        Tweet.objects.create(content='third tweet', user=self.secondUser)
+        self.user = User.objects.create_user(username="abc", password="password")
+        self.secondUser = User.objects.create_user(username="xyz", password="password")
+        Tweet.objects.create(content="first tweet", user=self.user)
+        Tweet.objects.create(content="second tweet", user=self.user)
+        Tweet.objects.create(content="third tweet", user=self.secondUser)
         self.currentCount = Tweet.objects.all().count()
 
     def test_tweet_created(self):
-        tweet_obj = Tweet.objects.create(content='tweet', user=self.user)
+        tweet_obj = Tweet.objects.create(content="tweet", user=self.user)
         self.assertEqual(tweet_obj.id, 4)
         self.assertEqual(tweet_obj.user, self.user)
 
     def get_client(self):
         client = APIClient()
-        client.login(username=self.user.username, password='password')
+        client.login(username=self.user.username, password="password")
         return client
 
     def test_tweet_list(self):
         client = self.get_client()
-        response = client.get('/api/tweets/')
+        response = client.get("/api/tweets/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 3)
+        self.assertEqual(len(response.json()["results"]), 3)
 
     def test_tweets_related_name(self):
         user = self.user
@@ -36,8 +36,8 @@ class TweetTestCase(TestCase):
 
     def test_action_like(self):
         client = self.get_client()
-        response = client.post('/api/tweets/action/', {'id': 1, 'action': 'like'})
-        like_count = response.json().get('likes')
+        response = client.post("/api/tweets/action/", {"id": 1, "action": "like"})
+        like_count = response.json().get("likes")
         user = self.user
         like_instances_count = user.tweetlike_set.count()
         related_likes = user.tweet_user.count()
@@ -48,45 +48,82 @@ class TweetTestCase(TestCase):
 
     def test_action_unlike(self):
         client = self.get_client()
-        response = client.post('/api/tweets/action/', {'id': 2, 'action': 'like'})
+        response = client.post("/api/tweets/action/", {"id": 2, "action": "like"})
         self.assertEqual(response.status_code, 200)
-        response = client.post('/api/tweets/action/', {'id': 2, 'action': 'unlike'})
+        response = client.post("/api/tweets/action/", {"id": 2, "action": "unlike"})
         self.assertEqual(response.status_code, 200)
-        like_count = response.json().get('likes')
+        like_count = response.json().get("likes")
         self.assertEqual(like_count, 0)
 
     def test_action_retweet(self):
         client = self.get_client()
-        response = client.post('/api/tweets/action/', {'id': 2, 'action': 'retweet'})
+        response = client.post("/api/tweets/action/", {"id": 2, "action": "retweet"})
         self.assertEqual(response.status_code, 201)
         data = response.json()
-        new_tweet_id = data.get('id')
+        new_tweet_id = data.get("id")
         self.assertNotEqual(2, new_tweet_id)
         self.assertEqual(self.currentCount + 1, new_tweet_id)
 
     def test_tweet_create_api_view(self):
-        request_data = {'content': 'test tweet'}
+        request_data = {"content": "test tweet"}
         client = self.get_client()
-        response = client.post('/api/tweets/create/', request_data)
+        response = client.post("/api/tweets/create/", request_data)
         self.assertEqual(response.status_code, 201)
         response_data = response.json()
-        new_tweet_id = response_data.get('id')
+        new_tweet_id = response_data.get("id")
         self.assertEqual(self.currentCount + 1, new_tweet_id)
 
     def test_tweet_detail_api_view(self):
         client = self.get_client()
-        response = client.get('/api/tweets/1/')
+        response = client.get("/api/tweets/1/")
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        _id = data.get('id')
+        _id = data.get("id")
         self.assertEqual(_id, 1)
 
     def test_tweet_delete_api_view(self):
         client = self.get_client()
-        response = client.delete('/api/tweets/1/delete/')
-        self.assertEqual(response.status_code, 200)
+        response = client.delete("/api/tweets/1/delete/")
+        self.assertEqual(response.status_code, 204)
         client = self.get_client()
-        response = client.delete('/api/tweets/1/delete/')
+        response = client.delete("/api/tweets/1/delete/")
         self.assertEqual(response.status_code, 404)
-        response_incorrect_owner = client.delete('/api/tweets/3/delete/')
+        response_incorrect_owner = client.delete("/api/tweets/3/delete/")
         self.assertEqual(response_incorrect_owner.status_code, 401)
+
+    def test_tweet_list_view_by_username(self):
+        client = self.get_client()
+        response = client.get("/api/tweets/", {"username": "abc"})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data["results"]), 2)
+
+    def test_tweet_feed_view(self):
+        client = self.get_client()
+        response = client.get("/api/tweets/feed/")
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data["results"]), 2)
+
+        # making user abc follow user xyz
+        second_user_profile = self.secondUser.profile
+        self.user.following.add(second_user_profile)
+        response = client.get("/api/tweets/feed/")
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data["results"]), 3)
+
+    def test_get_paginated_queryset_response(self):
+        for _ in range(25):
+            Tweet.objects.create(content="tweet", user=self.user)
+
+        client = self.get_client()
+        response = client.get("/api/tweets/")
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data["results"]), 20)
+
+        response = client.get("/api/tweets/", {"page": 2})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(len(data["results"]), 8)
