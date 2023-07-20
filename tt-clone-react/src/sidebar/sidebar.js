227ae79554bc7bd2d3e8cdb30419f3cccd1e167d
m-wilkosz <wilkosz1998@gmail.com>
2023-07-20 11:23:14 +0200

    logout functionality

--- tt-clone-react/src/sidebar/sidebar.js
+++ tt-clone-react/src/sidebar/sidebar.js
@@ -13,7 +13,7 @@ const SidebarComponent = ({currentUser}) => {
         <li>Messages</li>
         <li><Link to={"/bookmarks"} style={{textDecoration: "none", color: "inherit"}}>Bookmarks</Link></li>
         {currentUser && <li><Link to={`/profiles/${currentUser.username}`} style={{textDecoration: "none", color: "inherit"}}>Profile</Link></li>}
-        <li>Logout</li>
+        <li><a href="http://localhost:8000/logout/" className="logout-link" style={{textDecoration: "none", color: "inherit"}}>Logout</a></li>
       </ul>
     </div>
   )
